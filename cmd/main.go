8981aff06d1cfba8781203b9d3c454ab197b090a
kolibri7557 <kolibri557@gmail.com>
2023-06-17 16:25:21 +0300

    handler init

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	tryrest "github.com/kolibri7557/try-rest-api"
+	handler "github.com/kolibri7557/try-rest-api/pkg/handler"
+)
+
 func main() {
+	handlers := new(handler.Handler)
+	srv := new(tryrest.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 
 }
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package tryrest
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
@@ -9,12 +10,17 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error {
+func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
+		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
 	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
 }
