6d6a3ebc88878c3273fd6276f8ab94603368e6d6
kolibri7557 <kolibri557@gmail.com>
2023-08-10 08:53:13 +0500

    add migrations db

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users
+CREATE TABLE users 
 (
     id serial not null unique,
     name varchar(255) not null,
@@ -6,14 +6,31 @@ CREATE TABLE users
     password_hash varchar(255) not null
 );
 
-CREATE TABLE todo_lists
+CREATE TABLE todo_lists 
 (
     id serial not null unique,
     title varchar(255) not null,
     description varchar(255)
 );
 
-CREATE TABLE users_lists
+CREATE TABLE users_lists 
 (
 id serial not null unique,
+user_id int references users (id) on delete cascade not null,
+list_id int references todo_lists (id) on delete cascade not null
+);
+
+CREATE TABLE todo_items 
+(
+    id serial not null unique,
+    title varchar(255) not null,
+    description varchar(255),
+    done boolean not null default false
+);
+
+CREATE TABLE lists_items 
+(
+    id serial not null unique,
+    list_id int references todo_lists (id) on delete cascade not null,
+    item_id int references todo_items (id) on delete cascade not null
 );
